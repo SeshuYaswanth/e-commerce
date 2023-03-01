f8e8146cdaba46b08111f520941b211dd8eb8c20
Seshu yaswanth Reddy <97425325+SeshuYaswanth@users.noreply.github.com>
2023-03-01 07:54:10 +0530

    updated files

---
+++ E-cart/src/Data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Sweat shirt",
+      price: 3500,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-1.avif",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "casual Shirt",
+      price: 2300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-2.avif",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 2500,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-3.avif",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 3900,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/image-4.webp",
+    },
+    {
+        id: "thyfhcbcv",
+        name: "Mens Suit",
+        price: 3900,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/image-4.webp",
+    },
+  ];
